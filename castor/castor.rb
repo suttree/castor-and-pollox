8e29fa02737e902434a10e3684297c951a796379
Duncan Gough <duncan@suttree.com>
2012-04-07 18:14:25 +0100

    Html entities, etc...

--- castor/castor.rb
+++ castor/castor.rb
@@ -7,8 +7,6 @@ require 'htmlentities'
 
 class Castor
   def self.taller
-    coder = HTMLEntities.new
-
     @config = YAML::load(File.open('/home/suttree/public_html/troisen.com/gemini/config/castor.yml'))
 
     url = @config['urls'].sort_by{ rand }[0]
@@ -20,8 +18,11 @@ class Castor
     doc = Nokogiri::HTML(open(page))
     div = doc.css('div#all')[0].css('ul')[0]
 
+    url = entry.url
+    title = Castor::tidy(entry.title)
+
     story = '<li>'
-    story += "<a href='#{entry.url}' target='_blank'>#{coder.encode(Castor::truncate(entry.title, 70))}</a>"
+    story += "<a href='#{url}' target='_blank'>#{Castor::truncate(title, 75)}</a>"
     story += '</li>'
     
     div.add_child(story)
@@ -37,6 +38,14 @@ class Castor
     else 
       string
     end
+  end
+
+  def self.tidy(text)
+    coder = HTMLEntities.new
+    coder.encode(text)
+
+    text.gsub!(/\n/, ' ')
+    text.scan(/[[:print:]]/).join
   end
 end
 
